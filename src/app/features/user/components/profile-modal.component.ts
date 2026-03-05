fa64288be757b22d48732f4b903477dffad0b3f3
Aleksander Dokken <aleksdokken@gmail.com>
2026-03-05 15:06:27 +0100

    create Modal for displaying past bets

---
+++ src/app/features/user/components/profile-modal.component.ts
@@ -0,0 +1,18 @@
+import { CurrencyPipe, DatePipe, DecimalPipe, UpperCasePipe } from '@angular/common';
+import { Component, inject, output } from '@angular/core';
+import { UserService } from '../../betting/services/user.service';
+
+@Component({
+  selector: 'app-profile-modal',
+  imports: [CurrencyPipe, DatePipe, DecimalPipe, UpperCasePipe],
+  templateUrl: './profile-modal.component.html',
+  styleUrls: ['./profile-modal.component.css'],
+})
+export class ProfileModalComponent {
+  userService = inject(UserService);
+
+  closeModal = output<void>();
+  close() {
+    this.closeModal.emit();
+  }
+}
